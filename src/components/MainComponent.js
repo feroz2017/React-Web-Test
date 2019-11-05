264714b1bd0f7cea5bcf7933358ee34334509a25
Muhammad Feroz <mferoz.bscs17seecs@seecs.edu.pk>
2019-11-05 11:51:21 +0000

    Continuing adding routes

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,30 +1,35 @@
 import React ,{Component} from 'react'
+import Home  from './HomeComponent'
 import Menu from './MenuComponent'
 import {DISHES} from '../shared/dishes'
 import DishDetail from './DishdetailComponent'
 import Header from './HeaderComponent'
 import Footer from './FooterComponent'
+import {Route,Switch,Redirect} from 'react-router-dom';
 
 class Main extends React.Component {
     constructor(props){
       super(props)
       this.state = {
-        selectedDish : null,
+       
         dishes : DISHES
       }
     }
-    onDishSelect(dishId){
-        this.setState({
-            selectedDish:dishId
-        })
-    }
+   
   render(){
+    const HomePage=()=>{
+      return(
+        <Home/>
+      )
+    }
     return (
       <div>
         <Header/>
-      <Menu dishes={this.state.dishes} onClick={(dishId)=>this.onDishSelect(dishId)}/>
-      <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
-      <Footer/>
+          <switch>
+              <Route path="/home" component={HomePage}/>
+              <Route  exact path="/menu" component={()=> <Menu dishes={this.state.dishes}/>}/>
+          </switch>
+        <Footer/>
     </div>
     )
   }
